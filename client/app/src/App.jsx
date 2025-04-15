4e711d116c35ab3abd5815b014977a642f1c08e3
Vitor Souza <vitorpvp22@gmail.com>
2025-04-15 00:42:35 -0400

    delete and change
    including delete and change functions

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   const [bookTitle, setBookTitle] = useState("");
   const [releaseYear, setReleaseYear] = useState(0)
 
+  const [newTitle, setNewTitle] = useState("");
+
   useEffect(() => {
     fetchBooks();
   }, []);
@@ -13,10 +15,73 @@ function App() {
 
   const fetchBooks = async () => {
     try {
-      const response =  await fetch('http://127.0.0.1:8000/api/books/');
+      const response =  await fetch("http://127.0.0.1:8000/api/books/");
       const data = await response.json();
       setBooks(data);
     } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addBook = async () => {
+    const bookData = {
+      bookTitle,
+      release_year: releaseYear,
+    };
+    try {
+      const response =  await fetch("http://127.0.0.1:8000/api/books/create/",{
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bookData),
+      });
+
+      const data = await response.json();
+      setBooks((prev) => [...prev, data]);
+    } catch(err) {
+      console.log(err);
+    }
+
+  };
+
+  const updateBookTitle = async (pk, release_year) => {
+    const bookData = {
+      bookTitle: newTitle,
+      release_year: release_year,
+    };
+    try {
+      const response =  await fetch(`http://127.0.0.1:8000/api/books/${pk}/`, {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bookData),
+      });
+
+      const data = await response.json();
+      setBooks((prev) =>
+        prev.map((book) => {
+          if (book.id === pk) {
+            return data;
+          }else {
+          return book;
+          }
+        })
+      );
+    } catch(err) {
+      console.log(err);
+    }
+  };
+
+  const deleteBook = async (pk) => {
+    try {
+      const response =  await fetch(`http://127.0.0.1:8000/api/books/${pk}/`, {
+        method: "DELETE", 
+      });
+
+      setBooks((prev) => prev.filter((book) => book.id !== pk));
+    } catch(err) {
       console.log(err);
     }
   };
@@ -27,15 +92,28 @@ function App() {
       <h1>Book website </h1>
 
       <div>
-        <input type="text" placeholder="Book Title..." />
-        <input type="number" placeholder="Release year..." />
-        <button> Add Book </button>
+        <input type="text" placeholder="Book Title..."
+        onChange={(e) =>setBookTitle(e.target.value)}
+        />
+        <input type="number" placeholder="Release year..."
+        onChange={(e) =>setReleaseYear(e.target.value)}
+        />
+
+        <button onClick={addBook}> Add Book </button>
       </div>
       {books.map((book) => (
         <div>
           <p>Title: {book.bookTitle}</p>
           <p>Release Year: {book.release_year}</p>
-
+          <input type="text" placeholder="New Title..."
+          onChange={(e) =>setNewTitle(e.target.value)}
+          />
+          <button onClick={() => updateBookTitle(book.id, book.release_year)}>
+            Change title
+          </button>
+          <button  onClick={() => deleteBook(book.id)}>
+            Delete
+          </button>
       </div>
       ))}
     </>
--- server/newproject/api/views.py
+++ server/newproject/api/views.py
@@ -20,3 +20,22 @@ def create_book(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED )
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST )
+
+@api_view(['PUT', 'DELETE'])
+def book_detail(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'DELETE':
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    elif request.method == 'PUT':
+        data = request.data
+    serializer = BookSerializer(book, data =data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST )
