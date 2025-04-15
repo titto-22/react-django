0a612615ee2b44e62fe30131fcc34a7aafa659c5
Vitor Souza <vitorpvp22@gmail.com>
2025-04-14 23:18:23 -0400

    add

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,7 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [bookTitle, setBookTitle] = useState("");
+  const [releaseYear, setReleaseYear] = useState(0)
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+
+  const fetchBooks = async () => {
+    try {
+      const response =  await fetch('http://127.0.0.1:8000/api/books/');
+      const data = await response.json();
+      setBooks(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
 
@@ -9,9 +28,16 @@ function App() {
 
       <div>
         <input type="text" placeholder="Book Title..." />
-        <input type="number" placeholder="Release Date..." />
+        <input type="number" placeholder="Release year..." />
         <button> Add Book </button>
       </div>
+      {books.map((book) => (
+        <div>
+          <p>Title: {book.bookTitle}</p>
+          <p>Release Year: {book.release_year}</p>
+
+      </div>
+      ))}
     </>
   );
 }
--- server/newproject/newproject/settings.py
+++ server/newproject/newproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'newproject.urls'
@@ -123,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
